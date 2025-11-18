5996519e32ad728b5e0885c6b6830d8ca64eaf7f
SUMYATTHU1124 <thusumyat1124@gmail.com>
2025-11-18 18:54:05 +0630

    Dashboard Setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,45 +1,182 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Restaurant Order Management</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('src/assets/vendors/feather/feather.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('src/assets/vendors/ti-icons/css/themify-icons.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('src/assets/vendors/css/vendor.bundle.base.css') }}">
+    <link rel="stylesheet" href="{{ asset('src/assets/vendors/fontawesome-free-6.7.2-web/css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('src/assets/vendors/fontawesome-free-6.7.2-web/css/solid.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('src/assets/vendors/fontawesome-free-6.7.2-web/css/regular.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('src/assets/vendors/mdi/css/materialdesignicons.min.css') }}"> --}}
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    {{-- summer note css  --}}
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.20/dist/summernote-lite.min.css" rel="stylesheet">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- select2 --}}
+    <link rel="stylesheet" href="{{ asset('src/assets/css/select2.min.css') }}">
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <x-banner />
+    {{-- date time flatpickr  --}}
+    <link rel="stylesheet" href="{{ asset('src/assets/css/flatpickr.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('src/assets/css/flatpickr-month-year.css') }}">
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+    {{-- <script src="https://js.pusher.com/8.4.0/pusher.min.js"></script> --}}
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    {{-- for dashboard css ui  --}}
+    <link rel="stylesheet" href="{{ asset('src/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('src/assets/css/toastr.min.css') }}">
+
+    <!-- Summernote CSS -->
+
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    @yield('css')
+</head>
+
+<body>
+
+    <div class="loader-container">
+        <div class="loader-content">
+            <span class="loader"></span>
         </div>
+    </div>
 
-        @stack('modals')
+    {{-- navbar --}}
+    <div class="container-fluid page-body-wrapper">
+        @include('layouts.navbar')
+        {{-- sidebar --}}
+        @include('layouts.sidebar')
+        <div class="main-panel">
+            <div class="container">
+                <div class="row">
+                    <div class="col-10">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+            <!-- content-wrapper ends -->
+            <!-- partial:partials/_footer.html -->
+            {{-- <footer class="footer">
+                <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2025.
+                        <a href="#" target="_blank">AUSphere</a>. All rights reserved.</span>
+                    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with
+                        <i class="ti-heart text-danger ms-1"></i></span>
+                </div>
+            </footer> --}}
+            <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+    </div>
 
-        @livewireScripts
-    </body>
+    {{-- vendor.bundle.base.js is jquery and bootstrap file bundle --}}
+    <script src="{{ asset('src/assets/vendors/js/vendor.bundle.base.js') }}"></script>
+
+    <script src="{{ asset('src/assets/js/template.js') }}"></script>
+    <script src="{{ asset('src/assets/js/off-canvas.js') }}"></script>
+    {{-- toastr js  --}}
+    {{-- sweetalert2 --}}
+    <script src="{{ asset('src/assets/js/sweetalert2@11.js') }}"></script>
+
+    <script src="{{ asset('src/assets/js/toastr.min.js') }}"></script>
+
+    <!-- Summernote Bootstrap 5 compatible version by community-->
+
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.20/dist/summernote-lite.min.js"></script>
+
+    {{-- select 2 --}}
+    <script src="{{ asset('src/assets/js/select2.min.js') }}"></script>
+
+    {{-- date time flatpickr  --}}
+    <script src="{{ asset('src/assets/js/flatpickr.js') }}"></script>
+    <script src="{{ asset('src/assets/js/flatpickr-month-year.js') }}"></script>
+
+
+    {{-- @include('layouts.sweetalert-message') --}}
+    {{-- <script src="{{ asset('src/assets/js/pusher.js') }}"></script> --}}
+    {{-- <script src="{{ asset('src/assets/js/pusher.min.js') }}"></script> --}}
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pusher/7.0.3/pusher.min.js"></script>
+
+    <script src="{{ asset('js/custom.js') }}"></script>
+
+
+
+
+    @stack('js')
+
+
+    {{-- <script>
+        @if (auth()->check())
+            // Pusher.logToConsole = true;
+            var user = @json(auth()->user());
+            var pusher = new Pusher('{{ env('PUSHER_APP_KEY') }}', {
+                cluster: '{{ env('PUSHER_APP_CLUSTER') }}',
+                encrypted: true,
+                forceTLS: true,
+            });
+
+            var channel = pusher.subscribe('my-channel-test.' + user.id);
+
+            channel.bind('comment-notification', function(data) {
+                var badge = document.getElementById('notification-count');
+
+                if (badge) {
+                    var notiCount = parseInt(badge.innerText) || 0;
+                    notiCount++;
+                    badge.innerText = notiCount;
+
+                    // remove 'd-none' class to show badge
+                    badge.classList.remove('d-none');
+                }
+
+                // console.log('New notification:', data.message);
+            });
+
+            var chatChannel = pusher.subscribe('my-channel-test.' + user.id);
+
+            chatChannel.bind('chat-notification', function(data) {
+                console.log('Chat notification received', data);
+                var chatBadge = document.getElementById('chat-notification-count');
+
+                if (chatBadge) {
+                    var chatNotiCount = parseInt(chatBadge.innerText) || 0;
+                    chatNotiCount++;
+                    chatBadge.innerText = chatNotiCount;
+
+                    // remove 'd-none' class to show chatBadge
+                    chatBadge.classList.remove('d-none');
+                }
+
+                // console.log('New chat notification:', data.message);
+            });
+        @endif
+    </script> --}}
+    {{--
+   <script>
+
+//     // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+     var pusher = new Pusher('4e8eae8a55fa0aa4492d', {
+       cluster: 'ap1'
+     });
+
+     var channel = pusher.subscribe('my-channel-test');
+     channel.bind('my-event-test', function(data) {
+       alert(JSON.stringify(data));
+        });
+   </script> --}}
+
+
+</body>
+
 </html>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,77 @@
+<!-- partial:partials/_navbar.html -->
+<nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+    <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
+        <span class="fs-3 text-nowrap fw-bolder" style="z-index: 999; color: #E91E63">Logo</span>
+    </div>
+    <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+            <i class="icon-menu"></i>
+        </button>
+        <ul class="navbar-nav navbar-nav-right">
+            {{-- <li class="nav-item dropdown me-3">
+                <a class="nav-link count-indicator noti-icon dropdown-toggle position-relative" id="notificationDropdown"
+                    data-bs-toggle="dropdown">
+                    <i class="fa-regular fa-bell  menu-icon"></i>
+                    <!-- Badge positioned at top-right corner -->
+
+                    <!-- Comment notifications badge -->
+                    @php
+                        $commentNotiCount = auth()->user()->unreadNotifications()->where('type', 'App\Notifications\CommentNotification')->count();
+                    @endphp
+                    <span class="badge bg-danger rounded-pill position-absolute top-0 translate-middle {{ $commentNotiCount > 0 ? '' : 'd-none' }}"
+                        id="notification-count" style="margin-left: -30px;">
+                        {{ $commentNotiCount > 0 ? $commentNotiCount : 0 }}
+                    </span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list"
+                    aria-labelledby="notificationDropdown">
+                    <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
+                    <div id="noti_list">
+
+                    </div>
+                </div>
+            </li> --}}
+            {{-- <li class="nav-item dropdown me-3">
+                <a class="nav-link count-indicator chat-noti-icon dropdown-toggle position-relative" id="notificationDropdown"
+                    data-bs-toggle="dropdown">
+                     <i class="fa-regular fa-message"></i>
+
+                </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list"
+                    aria-labelledby="notificationDropdown">
+                    <p class="mb-0 font-weight-normal float-left dropdown-header">Message Notifications</p>
+                    <div id="chat_noti_list">
+
+                    </div>
+                </div>
+            </li> --}}
+
+            <li class="nav-item nav-profile dropdown">
+                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
+                    <span class="">{{ Auth::user()->name }}</span>
+                    <img src="{{ asset('src/assets/images/default-user-image.svg') }}" alt="profile" />
+                    {{-- <img src="{{ asset('storage/'.  $LoggedUserInfo->picture) }}" alt="profile" /> --}}
+                </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+                    <a class="dropdown-item">
+                        <i class="ti-settings text-primary"></i> Settings </a>
+                    <form action="{{ route('logout') }}" method="POST" class="dropdown-item">
+                        @csrf
+                        <button type="submit" class="btn btn-pink">Logout</button>
+                    </form>
+
+                </div>
+            </li>
+            {{-- <li class="nav-item nav-settings d-none d-lg-flex">
+                <a class="nav-link" href="#">
+                    <i class="icon-ellipsis"></i>
+                </a>
+            </li> --}}
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
+            data-toggle="offcanvas">
+            <span class="icon-menu"></span>
+        </button>
+    </div>
+</nav>
+<!-- partial -->
